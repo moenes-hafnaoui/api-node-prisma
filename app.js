728f009dec8765ce7c28c4afd7b34fee50691f36
Moenes Hafnaoui <moenesa7x@gmail.com>
2025-12-30 20:11:23 +0100

    local correction

--- app.js
+++ app.js
@@ -22,5 +22,4 @@ app.use("/api/articles", articlesRouter);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-
 module.exports = app;
